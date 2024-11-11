4a98bcc6a04ad2bf3845600a9ae4d993c3d4390e
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-11 18:44:38 -0500

    ✨ Add card shuffle and unique IDs

--- src/App.js
+++ src/App.js
@@ -2,43 +2,44 @@ import { useState } from 'react';
 import './styles/App.css';
 import { useEffect } from 'react';
 
-const cardList =[
-  {"path":"/img/1.jpeg"},
-  {"path":"/img/2.jpeg"},
-  {"path":"/img/3.jpeg"},
-  {"path":"/img/4.jpeg"},
-  {"path":"/img/5.jpeg"},
-  {"path":"/img/6.jpeg"},
+const cardList = [
+  { path: '/img/1.jpeg' },
+  { path: '/img/2.jpeg' },
+  { path: '/img/3.jpeg' },
+  { path: '/img/4.jpeg' },
+  { path: '/img/5.jpeg' },
+  { path: '/img/6.jpeg' },
 ];
 
 function App() {
-const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
 
-const prepareCards =() =>{
-  setCards([...cardList, ...cardList]);
-}
+  const prepareCards = () => {
+  
+    const sortedCards = [[...cardList, ...cardList]].sort(()=>0.5 - Math.random()).map((card)=>({...card, id:Math.random()}))
 
-useEffect(() => {
- prepareCards();
-}, [])
+    setCards(sortedCards);
+    
+  };
 
+  useEffect(() => {
+    prepareCards();
+  }, []);
 
-  return  <div className="container">
+  return (
+    <div className='container'>
       <h1>Memory App</h1>
       <button>Let's Play</button>
-
-
-      <div className='card-grid' >
-{
-  cards.map((card) => (
-    <div className='card' >
-      <img className='cardFront' src={card.path} alt="" />
-      <img className='cardBack' src="/img/cover.jpeg" alt="" />
-    </div>
-  ))
-}
+      <div className='card-grid'>
+        {cards.map((card) => (
+          <div className='card'>
+            <img className='cardFront' src={card.path} alt='' />
+            <img className='cardBack' src='/img/cover.jpeg' alt='' />
+          </div>
+        ))}
       </div>
     </div>
+  );
 }
 
 export default App;
